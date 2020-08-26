3aef2a87ea8be26934a860427730f35be95023b9
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-08-26 10:09:31 -0600

    Create a method to test total area of home and class method to calculate total area

--- lib/house.rb
+++ lib/house.rb
@@ -17,4 +17,16 @@ class House
     @price > 500000
   end
 
+  def rooms_from_category(category)
+    @rooms.find_all do |room|
+      room.category == category
+    end
+  end
+
+  def area
+    @rooms.sum do |room|
+      room.area
+    end
+  end
+
 end
