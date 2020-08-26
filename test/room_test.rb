acea1edb811e97bd3b65287c8e7250ac89685bc8
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-08-26 09:15:57 -0600

    Create a method to test room is created not painted

--- test/room_test.rb
+++ test/room_test.rb
@@ -19,4 +19,10 @@ class RoomTest < Minitest::Test
     assert_equal 130, room1.area
     assert_equal 180, room2.area
   end
+
+  def test_it_is_created_not_painted
+    room = Room.new(:bedroom, 10, '13')
+
+    refute room.is_painted?
+  end
 end
